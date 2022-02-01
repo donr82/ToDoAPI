67e4bacbba74816d0d2f0ba10d3975fe564665cc
Don Richardson <donrichardson092388@outlook.com>
2022-02-01 15:28:06 -0600

    Get ToDos completed

--- ToDoAPI/ToDoAPI.API/Controllers/ToDoController.cs
+++ ToDoAPI/ToDoAPI.API/Controllers/ToDoController.cs
@@ -10,7 +10,34 @@ using System.Web.Http.Cors;
 
 namespace ToDoAPI.API.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ToDoController : ApiController
     {
-    }
-}
+        ToDoEntities db = new ToDoEntities();
+
+        public IHttpActionResult GetToDos()
+        {
+            List<ToDoItemAPIViewModels> todos = db.Todoitems.Include("Category").Select(r => new ToDoItemAPIViewModels()
+            {
+
+                Todoid = r.Todoid,
+                Action = r.Action,
+                Done = r.Done,
+                CategoryId = r.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = r.Category.CategoryId,
+                    Name = r.Category.Name,
+                    Description = r.Category.Description,
+                }
+            }).ToList<ToDoItemAPIViewModels>();
+
+            if (todos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(todos);
+        }//end GetToDos
+    }//end ToDoController
+}//End namespace
